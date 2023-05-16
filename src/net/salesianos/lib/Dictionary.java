ac683f695cd00937f0bdbade5609af5554df84bd
AdanYanes <usr3613@salesianos-lacuesta.net>
2023-05-16 18:05:05 +0100

    feat(Dictionary): added searchWord method.

--- src/net/salesianos/lib/Dictionary.java
+++ src/net/salesianos/lib/Dictionary.java
@@ -3,9 +3,12 @@ package net.salesianos.lib;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Dictionary {
+
+    Scanner scanner = new Scanner(System.in);
 
     Map<String, Set<String>> dictionary;
     
@@ -30,6 +33,24 @@ public class Dictionary {
             Set<String> newValues = new HashSet<>();
             newValues.add(word);
             dictionary.put(initial, newValues);
+        }
+    }
+
+    public void searchWord(){
+        System.out.println("Por favor, introduzca la palabra que desea buscar");
+        String word = scanner.nextLine().replaceAll("[\\s0-9\\p{Punct}]", "").toLowerCase();
+
+        String initial = String.valueOf(word.charAt(0)).toUpperCase();
+
+        if(checkIfInitialExist(initial)){
+            Set<String> nextValues = dictionary.get(initial);
+            if(nextValues.contains(word)){
+                System.out.println("La palabra introducida se encuentra en el diccionario");
+            }else{
+                System.out.println("La palabra introducida no se encuentra en el diccionario");
+            }
+        }else{
+            System.out.println("La palabra introducida no se encuentra en el diccionario");
         }
     }
 
